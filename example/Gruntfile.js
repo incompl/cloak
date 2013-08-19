a7b385e68f3c5af0a0988a38c3362785c32e0285
Darius Kazemi <darius.kazemi@gmail.com>
2013-08-19 15:25:45 -0400

    example game

---
+++ example/Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      app: {
+        src: ['js/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      app: {
+        files: '<%= jshint.app.src %>',
+        tasks: ['jshint:app']
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
